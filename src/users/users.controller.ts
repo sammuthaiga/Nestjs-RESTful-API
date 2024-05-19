f1872e5c6d72eaceada0ba9d7b000ca833641796
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-05-19 20:47:44 +0300

    create users controller and service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users') //Parent route
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get() // GET /users
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Post() // POST /users
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
+  }
+
+  @Patch(':id') // PUT /users/:id
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(+id);
+  }
+}
